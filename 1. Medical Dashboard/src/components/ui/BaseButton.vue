f09fd79a120f96eb635a3c1f344c4e98316bedc8
PC <khusanov.it.tech@gmail.com>
2021-08-20 16:57:58 +0500

    main buttons

---
+++ 1. Medical Dashboard/src/components/ui/BaseButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button>{{ title }}</button>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  width: 24rem;
+  height: 4.6rem;
+  background: #fff;
+  border-radius: 6px;
+  border: none;
+  font-weight: 700;
+  font-size: 1.4rem;
+  font-family: inherit;
+  display: inline-block;
+  transition: .2s ease;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:active {
+    transform: translateY(3px);
+  }
+}
+</style>
