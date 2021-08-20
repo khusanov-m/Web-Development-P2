dec1679e41047956aa9a354cd9ad274d1f87a35f
PC <khusanov.it.tech@gmail.com>
2021-08-20 16:59:34 +0500

    navigation

---
+++ 1. Medical Dashboard/src/components/content/TheNavigation.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul class="navItems">
+    <nav-items
+      v-for="(navContent, index) in navContents"
+      :key="index"
+      :navContentsData="navContent"
+      :isActive="activeIndex === index"
+      @onToggle="onToggle(index)"
+    >
+    </nav-items>
+  </ul>
+</template>
+
+<script>
+import NavItems from "../ui/NavItems.vue";
+export default {
+  components: { NavItems },
+  data() {
+    return {
+      navContents: [
+        { title: "home", icon: "home.svg" },
+        { title: "patient", icon: "profile.svg" },
+        { title: "places", icon: "place.svg" },
+        { title: "data & Notes", icon: "notes.svg" },
+        { title: "Analytics", icon: "statistic.svg" },
+        { title: "Setting", icon: "gear.svg" },
+        { title: "log Out", icon: "out.svg" },
+      ],
+      activeIndex: null,
+    };
+  },
+  methods: {
+    onToggle(index) {
+      if (this.activeIndex === index) {
+        this.activeIndex = null;
+      } else {
+        this.activeIndex = index;
+      }
+    },
+  },
+  mounted: function() {
+    this.activeIndex = 0;
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.navItems {
+  margin-left: 1.6rem;
+}
+</style>
