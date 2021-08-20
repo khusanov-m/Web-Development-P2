d0ded0a1c0eccf65329772ad2be559fed5b0d2d6
PC <khusanov.it.tech@gmail.com>
2021-08-20 16:58:05 +0500

    minor elements

---
+++ 1. Medical Dashboard/src/components/ui/MiniElements.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mini-els">
+    <span></span>
+    <span></span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.mini-els {
+  display: flex;
+  cursor: pointer;
+
+  transition: 0.2s ease;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:active {
+    transform: translateY(3px);
+  }
+
+  span {
+    width: 5px;
+    height: 5px;
+    background: #0f3f62;
+    border-radius: 50%;
+
+    &:first-child {
+      margin-right: 1.4rem;
+    }
+  }
+}
+</style>
